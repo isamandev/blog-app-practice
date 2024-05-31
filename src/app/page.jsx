be15ada3dc81e3b5c0d3081eb76a68faaf08574d
SamanAbbasi <samanabbasi.plus@gmail.com>
2024-05-31 11:38:50 +0330

    install next js & set global styles

---
+++ src/app/page.jsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import styles from "./homepage.module.css";
+
+export default function Home() {
+  return (
+    <div>
+      <Link href="/">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia placeat
+        minima distinctio fugiat odio veritatis velit sint consequatur repellat
+        dolorem omnis beatae perferendis obcaecati dolores, consectetur ex iure
+        saepe tempore?
+      </Link>
+    </div>
+  );
+}
